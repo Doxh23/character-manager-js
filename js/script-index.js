48b8506c45a8c707f9e818f0221990d31f1a75a9
Adrien Péters <adrien.peters95@gmail.com>
2021-08-12 09:59:36 +0200

    js- htm-css main page (we need to do see character and search bar)

---
+++ js/script-index.js
@@ -0,0 +1,60 @@
+
+
+
+
+(async() => {
+
+   
+
+// console(data.find(names => names.name === "mike"))
+let heroes = await fetch(`https://character-database.becode.xyz/characters`)
+datas= await heroes.json()  
+document.getElementById("search-bar").addEventListener("keyup", function(e){
+const  searchstring = e.target.value
+let filteredCharacter = datas.filter(data => {
+    return data.name.includes(searchstring)
+}).name
+console.log(filteredCharacter)
+
+
+
+   
+
+   4
+
+})
+
+datas.forEach(obj => {
+    let temp= document.getElementsByTagName("template")[0];
+    let target= document.getElementById("target")
+    let clon = temp.content.cloneNode(true)
+    clon.querySelector("img").src= `data:image/png;base64,${obj.image}`
+    clon.querySelector("p").innerHTML= obj.shortDescription
+    clon.querySelector("h3").innerHTML= obj.name
+    target.appendChild(clon)
+
+})
+
+
+    
+document.getElementById("index-new-btn").addEventListener("click",function(){
+window.location.href="/character-manager-js/pages/manager-character.html"
+
+
+})
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let heroes = await fetch(`https://character-database.becode.xyz/characters`)

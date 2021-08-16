c67589bcb387ea7521151e6553fe77fa73983ed9
Adrien Péters <adrien.peters95@gmail.com>
2021-08-16 17:10:35 +0200

    fin js + fin css

--- js/script-index.js
+++ js/script-index.js
@@ -4,15 +4,11 @@
 let heroes = await fetch(`https://character-database.becode.xyz/characters`)
 datas= await heroes.json()  
 
-//document.getElementById("search-bar").addEventListener("keyup", function(e){
-//const  searchstring = e.target.value
-//let filteredCharacter = datas.filter(data => {
- //   return data.name.includes(searchstring)
-//}).name
-//console.log(filteredCharacter)
-//})
+
+
 
 // Clone, use and fill of the template and 
+
 datas.forEach(obj => {
     let temp= document.getElementsByTagName("template")[0];
     let target= document.getElementById("target");
@@ -22,9 +18,9 @@ datas.forEach(obj => {
     clone.querySelector("p").innerHTML= obj.shortDescription;
     clone.querySelector("h3").innerHTML= obj.name;
     clone.querySelector("a").href = `/character-manager-js/pages/single-character.html?${obj.id}`;
-
-    target.appendChild(clone)
+target.appendChild(clone)
 });
+
 
 // Switch to the Single Character page    
 document.getElementById("index-new-btn").addEventListener("click",function(){
@@ -33,15 +29,20 @@ window.location.href="/character-manager-js/pages/manager-character.html"
 
 })();
 
-
-
-
-
-
-
-
-
-
-
-
-
+/****************************************************SEARCH BAR*************************************************************** */
+document.getElementById("search-bar").addEventListener("keyup", function(){
+    let bar = document.getElementById("search-bar")
+     let searchstring = bar.value.toUpperCase()
+     console.log(searchstring)
+     let carte = document.querySelectorAll(".carte")
+     for (let i=0; i< carte.length; i++)  {
+         let h3 = carte[i].querySelector("h3");
+         let txt =  h3.textContent || h3.innerText;
+         if (txt.toUpperCase().indexOf(searchstring)> -1){
+             carte[i].style.display = ""
+         }
+         else {
+             carte[i].style.display = "none"
+         }
+     }
+    })
